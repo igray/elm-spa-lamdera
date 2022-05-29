73142b85c9fa1d42c779c842a61c341baee4efd4
Iain Gray <igray78756@gmail.com>
2022-05-28 19:48:43 -0500

    Initialized elm-review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,63 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+
+import Docs.ReviewAtDocs
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoSimpleLetBody
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule as Rule exposing (Rule)
+import Simplify
+
+
+config : List Rule
+config =
+    [ Docs.ReviewAtDocs.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-spa/" ]
+    , NoMissingTypeAnnotationInLetIn.rule
+    , NoMissingTypeExpose.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-spa/" ]
+    , NoSimpleLetBody.rule
+    , NoPrematureLetComputation.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Exports.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-spa/" ]
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-spa/" ]
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-spa/" ]
+    , Simplify.rule Simplify.defaults
+    ]
